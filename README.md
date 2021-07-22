74408aedf3f42691e8786400bc85e0edaf2df4fa
Anton Jansen <antonjansen96@gmail.com>
2021-07-22 14:47:58 +0200

    finish genparams for two-state

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-Misc to-do:
-* make the location/name of lambdagrouptypes.dat an optional variable on cmdline.
+To-do addparams:
+* Write the code.
+
+To-do later:
+* Add the support for multistate.
+* Check if everything has enough comments.
+* Check if everything has enough utils.update messages.
+* Set the appropriate verbosity levels for all the utils.update messages.
+* Sanitize input in parsecmdline.py and parselambda.py.
+* Write README manual.
+* Write better -h help message for everything.
+
+Done:
+* Implement this incl = thing from Pavel in lambdagrouptypes.dat.
+* Improve the pdb2gmx mess by prompting user if topology for unknown residue names is in force field (.rtp). If yes, add name to residuetypes.dat, if not give this link: https://manual.gromacs.org/current/how-to/topology.html and tell user to do it themselves.
+* Write addbuffers function.
--- genparams.py
+++ genparams.py
@@ -10,17 +10,21 @@ def genparams():
     # Load d_residues here for speedup as we need it in multiple loops.
     residues = universe.get('d_residues')
 
+    # Load ph_lambdaTypes here for speedup as we need it in multiple loops.
+    LambdaTypes = universe.get('ph_lambdaTypes')
+
+    # List of groupnames of the LambdaTypes specified in lambdagrouptypes.dat.
+    LamdaTypeNames = []
+    for LambdaType in LambdaTypes:
+        LamdaTypeNames.append(LambdaType.d_groupname)
+
     # Check whether we have any titratable residues in the structure, and also
     # check whether we have any buffers.
     anyTitratables = False
     restrainCharge = False
-    
-    lambdaTypeNamesSpecifed = []
-    for LambdaType in universe.get('ph_lambdaTypes'):
-        lambdaTypeNamesSpecifed.append(LambdaType.d_groupname)
 
     for residue in residues:
-        if residue.d_resname in lambdaTypeNamesSpecifed:
+        if residue.d_resname in LamdaTypeNames:
             anyTitratables = True
 
         elif residue.d_resname == 'BUF':
@@ -52,7 +56,7 @@ def genparams():
     def addParam(name, value):
             file.write("{:54s} = {:13s}\n".format(name, str(value)))
 
-    # PART 1 - WRITE GENERAL PARAMETERS ########################################
+    # PART 1 - WRITE GENERAL PARAMETERS
 
     file.write("\n; CONSTANT PH\n")
 
@@ -66,31 +70,40 @@ def genparams():
     if restrainCharge:
         addParam('lambda-dynamics-charge-constraints', 'yes')
 
-    # Gather a list of the names of the lambda residues in the protein.
-    lambdaResidueNameList = []
-    for residue in residues:
-        if (residue.d_resname in lambdaTypeNamesSpecifed):
-            lambdaResidueNameList.append(residue.d_resname)
+    # We need to count how many titratable residues in total we have in the
+    # protein. For this we compile a list LambdasFoundinProtein.
+    LambdasFoundinProtein = [] # (e.g ASPT ASPT GLUT ASPT GLUT ASPT...)
+    
+    # Stores the number of buffer atoms/ions.
+    buffersFoundinProtein = 0
 
-    # Gather a list of the names of the lambda residue-types IN THE PROTEIN,
-    # And make sure the order is the same as in ph_lambdaTypes = LambdaTypeNamesSpecified.
-    lambdaResidueTypeList = []
-    for obj in universe.get('ph_lambdaTypes'):
-        if (obj.d_groupname in set(lambdaResidueNameList)):
-            lambdaResidueTypeList.append(obj.d_groupname)
+    for residue in residues:
+        if residue.d_resname in LamdaTypeNames:
+            LambdasFoundinProtein.append(residue.d_resname)
+
+        # Also in this loop we count how many buffer ions we have.
+        elif restrainCharge and residue.d_resname == 'BUF':
+            buffersFoundinProtein += 1
+
+    # The fact that we have a LambdaType in lambdagrouptypes.dat does not mean
+    # one of those is also present in the protein. In that case, we want to 
+    # prevent counting it, so we compile a subgroup of LambdaType groupnames
+    # that are not only in lambdagrouptypes.dat, but ALSO found at least once 
+    # in the actual protein.
+    LambdaTypeNamesFoundinProtein = list(set(LambdasFoundinProtein)) # (e.g ASPT GLUT)
 
     # If we use the charge leveling scheme "charge-restraining" (2) we also have
     # the BUF residue-type as well as one extra lambda group containing all the BUFs.
     if restrainCharge:
-        addParam('lambda-dynamics-number-lambda-group-types', len(lambdaResidueTypeList) + 1)
-        addParam('lambda-dynamics-number-atom-collections', len(lambdaResidueNameList) + 1)
+        addParam('lambda-dynamics-number-lambda-group-types', len(LambdaTypeNamesFoundinProtein) + 1)
+        addParam('lambda-dynamics-number-atom-collections', len(LambdasFoundinProtein) + 1)
     else:
-        addParam('lambda-dynamics-number-lambda-group-types', len(lambdaResidueTypeList))
-        addParam('lambda-dynamics-number-atom-collections', len(lambdaResidueNameList))
+        addParam('lambda-dynamics-number-lambda-group-types', len(LambdaTypeNamesFoundinProtein))
+        addParam('lambda-dynamics-number-atom-collections', len(LambdasFoundinProtein))
 
     file.write('\n')
 
-    # # PART 2 - WRITE LAMBDA GROUP TYPES
+    # PART 2 - WRITE LAMBDA GROUP TYPES
     
     # Convert a list to a string
     def to_string(Input):
@@ -110,9 +123,12 @@ def genparams():
         file.write('\n')
 
     number = 1
-    for LambdaType in universe.get('ph_lambdaTypes'):
-        # This if-statement prevents writing a block when there are no residues of this type.
-        if (LambdaType.d_groupname in lambdaResidueTypeList):
+    # We loop over the object itself instead of the d_groupname as we need all
+    # the information in the object.
+    for LambdaType in LambdaTypes:
+        # This if-statement prevents writing a block when there are no residues 
+        # of this type in the protein.
+        if (LambdaType.d_groupname in LambdaTypeNamesFoundinProtein):
             writeBlock(number, 
                        LambdaType.d_groupname, 
                        LambdaType.d_dvdl[::-1],
@@ -129,8 +145,8 @@ def genparams():
                    universe.get('ph_BUF_dvdl')[::-1],
                    0,
                    0,
-                   1.0,
-                   0.0)
+                   [1.0],
+                   [0.0])
 
     # PART 3 - WRITE LAMBDA GROUPS
    
@@ -144,77 +160,66 @@ def genparams():
 
         if (name == 'BUF'):
             addParam('lambda-dynamics-atom-set{}-buffer-residue'.format(number), 'yes')
-            addParam('lambda-dynamics-atom-set{}-buffer-residue-multiplier'.format(number), universe.get('ph_bufnmol'))
+            addParam('lambda-dynamics-atom-set{}-buffer-residue-multiplier'.format(number), buffersFoundinProtein)
 
         file.write('\n')
 
     number = 1
-    for name in lambdaResidueNameList:
-        writeResBlock(number, name, 'LAMBDA{}'.format(number))
+    for groupname in LambdasFoundinProtein:
+        writeResBlock(number, groupname, 'LAMBDA{}'.format(number))
         number += 1
 
     if (restrainCharge):
-        writeResBlock(number, 'BUF', 'LAMBDA{}'.format(len(lambdaResidueNameList) + 1))
+        writeResBlock(number, 'BUF', 'LAMBDA{}'.format(len(LambdasFoundinProtein) + 1))
 
     file.close() # MD.mdp
 
     # PART 4 - WRITE LAMBBDA INDEX GROUPS
 
-    # If we use do charge restraining, we'll need a list of atomIndices of the BUFs:
-    if restrainCharge:
-        bufferAtomIndexList = []
-
-        atomidx = 1
-        for residue in residues:
-            for _ in residue.d_atoms:
-                if residue.d_resname == 'BUF':
-                    bufferAtomIndexList.append(atomidx)
-                atomidx += 1
-
     # Append to existing index.ndx
     file = open('index.ndx', 'a')
 
-    # Formatting file for writing the block.
+    # Formatting function for writing the index block.
     def writeTheGroup(number, atomIndexList):
         file.write('\n[ LAMBDA{} ]\n'.format(number))
         for index in atomIndexList:
             file.write('{} '.format(index))
         file.write('\n')
 
-    # ph_lambdaTypes = universe.get('ph_lambdaTypes')
+    atomCount   = 1
+    groupNumber = 1
+    bufferIndexList = []
 
-    # atomCount = 1; groupNumber = 1
-    # for residue in universe.get('d_residues'):
-    #     if residue.d_resname in lambdaResidueTypeList:
+    # Write the lambda index groups for the titratable residues
+    for residue in residues:
+        # If the residue is titratable
+        if residue.d_resname in LambdaTypeNamesFoundinProtein:
+            # To hold the atom indices corresponding to the titratable atoms            
+            atomIndexList = []
+            # Corresponding LambdaType object
+            LambdaType = [obj for obj in LambdaTypes if obj.d_groupname == residue.d_resname][0]
 
-    #         atomIndexList = []
-    #         obj = ph_lambdaTypes[lambdaTypeNamesSpecifed.index(residue.d_resname)]
+            # Loop through atoms - note that the atoms need to be descending order
+            for atom in residue.d_atoms:
+                if atom in LambdaType.d_atoms:
+                    atomIndexList.append(atomCount)
 
-    #         for atom in residue.d_atoms:
-    #             if atom in obj.d_atoms:
-    #                 atomIndexList.append(atomCount)
+                atomCount += 1
+            
+            # Write the lambda index group and increment groupnumber
+            writeTheGroup(groupNumber, atomIndexList)
+            groupNumber += 1
 
-    #             atomCount += 1
+        elif restrainCharge and residue.d_resname == 'BUF':
+            bufferIndexList.append(atomCount)
+            atomCount += 1
 
-    #         # If we use "charge-coupling" (1), assign the atomIndices of one BUF
-    #         # to one protonatable lambda residue (use clever list slicing):            
-    #         if (universe.get('ph_QQleveling') == 1):
-    #             start = (groupNumber - 1) * len(BUF_qqA)
-    #             stop  = start + len(BUF_qqA)
-    #             atomIndexList += bufferAtomIndexList[start:stop]                
+        else: # Increment atomCount
+            for atom in residue.d_atoms:
+                atomCount += 1
 
-    #         writeTheGroup(groupNumber, atomIndexList)
-    #         groupNumber += 1
-
-    #     else:
-    #         for atom in residue.d_atoms:
-    #             atomCount += 1
-
-    # If we use "charge-restraining" (2), add everything in bufferAtomIndexList
-    # to the last lambda index group:
-
-    # If we do charge restraining, write the buffer index group
+    # If we do charge restraining write the lambda index group for the buffer(s)
     if restrainCharge:
-        writeTheGroup(groupNumber, bufferAtomIndexList)
+        writeTheGroup(groupNumber, bufferIndexList)
 
     file.close() # index.ndx
