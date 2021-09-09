d9c1bd74b5ca35c64bf3979bb9c1714310896230
Anton Jansen <antonjansen96@gmail.com>
2021-09-09 13:02:34 +0200

    update plotlambda

--- analyze.py
+++ analyze.py
@@ -1,13 +1,30 @@
 #!/bin/python3
 
+import os
 import matplotlib.pyplot as plt
-import load
+import structure, universe
+
+def loadCol(fileName, col, start=0, stop=0):
+    data = []
+
+    try:
+        for x, y in enumerate(open(fileName)):
+            if start == 0 and stop == 0 and (y.split()[0][0] not in ['@','#','&']):
+                data.append(float(y.split()[col-1]))
+
+            elif (x >= start-1) and (x <= stop-1):
+                data.append(float(y.split()[col-1]))
+
+    except IndexError:
+        pass
+
+    return data
 
 def titrate(lambdaFileName, cutoff=0.80):
     lambda_proto   = 0
     lambda_deproto = 0
-    
-    for x in load.Col(lambdaFileName, 2):
+
+    for x in loadCol(lambdaFileName, 2):
         if (x > cutoff):
             lambda_deproto += 1
         if (x < 1 - cutoff):
@@ -37,15 +54,15 @@ def compareLambdaFiles(namelist):
 
     # Get the data and plot.
     for name in namelist:
-        time        = load.Col(name, 1)
-        lambda_x    = load.Col(name, 2)
-        lambda_dvdl = load.Col(name, 3)
-        lambda_temp = load.Col(name, 4)
-        lambda_vel  = load.Col(name, 5)
-        F_coulomb   = load.Col(name, 6)
-        F_corr      = load.Col(name, 7)
-        F_bias      = load.Col(name, 8)
-        F_ph        = load.Col(name, 9)
+        time        = loadCol(name, 1)
+        lambda_x    = loadCol(name, 2)
+        lambda_dvdl = loadCol(name, 3)
+        lambda_temp = loadCol(name, 4)
+        lambda_vel  = loadCol(name, 5)
+        F_coulomb   = loadCol(name, 6)
+        F_corr      = loadCol(name, 7)
+        F_bias      = loadCol(name, 8)
+        F_ph        = loadCol(name, 9)
 
         plt1.plot(time, lambda_x, linewidth=0.5, label="deprotonation = {:.2f}".format(titrate(name)))
         plt2.plot(time, lambda_temp, linewidth=0.5, label="mean = {:.1f} (K)".format(sum(lambda_temp)/len(lambda_temp)))
@@ -105,5 +122,387 @@ def compareLambdaFiles(namelist):
     # plt.tight_layout()
     plt.show()
 
-compareLambdaFiles(['lambda_1.dat', 'lambda_2.dat', 'lambda_3.dat'])
-compareLambdaFiles(['lambda_4.dat', 'lambda_5.dat', 'lambda_6.dat'])
+def plotlambda(fileName, plotHSPT=True, plotBUF=False):
+    structure.load(fileName)
+
+    idx = 1
+    for residue in universe.get('d_residues'):
+        if residue.d_resname in ['ASPT', 'GLUT']:
+            t = loadCol('lambda_{0}.dat'.format(idx), 1)
+            x = loadCol("lambda_{0}.dat".format(idx), 2)
+
+            plt.plot(t, x, label='{}-{}'.format(residue.d_resname, residue.d_resid), linewidth=1)
+            idx += 1
+
+        elif residue.d_resname == 'HSPT':
+            if plotHSPT:
+                for val in range(0, 3):
+                    t = loadCol('lambda_{0}.dat'.format(idx + val), 1)
+                    x = loadCol("lambda_{0}.dat".format(idx + val), 2)
+
+                    plt.plot(t, x, label='{}-{}-{}'.format(residue.d_resname, residue.d_resid, val+1), linewidth=1)
+
+            idx += 3
+
+    if plotBUF:
+        t = loadCol("lambda_{0}.dat".format(idx), 1)
+        x = loadCol("lambda_{0}.dat".format(idx), 2)
+
+        plt.plot(t, x, label="Buffer", linewidth=1)
+
+    plt.xlabel("Time (ps)")
+    plt.ylabel(r"$\lambda$-coordinate")
+    plt.ylim(-0.1, 1.1)
+    plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 3))
+
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def glicphstates(fileName, pdbName, pH, nstOut):
+    # EXPERIMENTAL DATA ON PROTONATION STATES AT VARIOUS PH ####################
+    biophys = { # also prevost2012
+        'ASPT-13'  : 1,
+        'ASPT-31'  : 1,
+        'ASPT-32'  : 1,
+        'ASPT-49'  : 1,
+        'ASPT-55'  : 1,
+        'ASPT-86'  : 0,
+        'ASPT-88'  : 0,
+        'ASPT-91'  : 1,
+        'ASPT-97'  : 1,
+        'ASPT-115' : 1,
+        'ASPT-122' : 1,
+        'ASPT-136' : 1,
+        'ASPT-145' : 1,
+        'ASPT-153' : 1,
+        'ASPT-154' : 1,
+        'ASPT-161' : 1,
+        'ASPT-178' : 1,
+        'ASPT-185' : 1,
+        'GLUT-14'  : 1,
+        'GLUT-26'  : 0,
+        'GLUT-35'  : 0,
+        'GLUT-67'  : 0,
+        'GLUT-69'  : 1,
+        'GLUT-75'  : 0,
+        'GLUT-82'  : 0,
+        'GLUT-104' : 1,
+        'GLUT-147' : 1,
+        'GLUT-163' : 1,
+        'GLUT-177' : 0,
+        'GLUT-181' : 1,
+        'GLUT-222' : 1,
+        'GLUT-243' : 0,
+        'GLUT-272' : 1,
+        'GLUT-282' : 1
+    }
+
+    nury2010 = { # this is also cheng2010, calimet2013
+        'ASPT-13'  : 1,
+        'ASPT-31'  : 1,
+        'ASPT-32'  : 1,
+        'ASPT-49'  : 1,
+        'ASPT-55'  : 1,
+        'ASPT-86'  : 0,
+        'ASPT-88'  : 0,
+        'ASPT-91'  : 1,
+        'ASPT-97'  : 1,
+        'ASPT-115' : 1,
+        'ASPT-122' : 1,
+        'ASPT-136' : 1,
+        'ASPT-145' : 1,
+        'ASPT-153' : 1,
+        'ASPT-154' : 1,
+        'ASPT-161' : 1,
+        'ASPT-178' : 1,
+        'ASPT-185' : 1,
+        'GLUT-14'  : 1,
+        'GLUT-26'  : 0,
+        'GLUT-35'  : 0,
+        'GLUT-67'  : 0,
+        'GLUT-69'  : 0,
+        'GLUT-75'  : 0,
+        'GLUT-82'  : 0,
+        'GLUT-104' : 1,
+        'GLUT-147' : 1,
+        'GLUT-163' : 1,
+        'GLUT-177' : 0,
+        'GLUT-181' : 1,
+        'GLUT-222' : 1,
+        'GLUT-243' : 0,
+        'GLUT-272' : 1,
+        'GLUT-282' : 1
+    }
+
+    fritsch2011 = {
+        'ASPT-13'  : 0,
+        'ASPT-31'  : 0,
+        'ASPT-32'  : 1,
+        'ASPT-49'  : 1,
+        'ASPT-55'  : 0,
+        'ASPT-86'  : 0,
+        'ASPT-88'  : 0,
+        'ASPT-91'  : 0,
+        'ASPT-97'  : 0,
+        'ASPT-115' : 1,
+        'ASPT-122' : 1,
+        'ASPT-136' : 1,
+        'ASPT-145' : 0,
+        'ASPT-153' : 0,
+        'ASPT-154' : 0,
+        'ASPT-161' : 0,
+        'ASPT-178' : 0,
+        'ASPT-185' : 0,
+        'GLUT-14'  : 0,
+        'GLUT-26'  : 0,
+        'GLUT-35'  : 0,
+        'GLUT-67'  : 0,
+        'GLUT-69'  : 0,
+        'GLUT-75'  : 0,
+        'GLUT-82'  : 0,
+        'GLUT-104' : 1,
+        'GLUT-147' : 0,
+        'GLUT-163' : 0,
+        'GLUT-177' : 0,
+        'GLUT-181' : 0,
+        'GLUT-222' : 1,
+        'GLUT-243' : 0,
+        'GLUT-272' : 0,
+        'GLUT-282' : 0
+    }
+
+    lev2017 = {
+        'ASPT-13'  : 1,
+        'ASPT-31'  : 1,
+        'ASPT-32'  : 1,
+        'ASPT-49'  : 1,
+        'ASPT-55'  : 1,
+        'ASPT-86'  : 1,
+        'ASPT-88'  : 1,
+        'ASPT-91'  : 1,
+        'ASPT-97'  : 1,
+        'ASPT-115' : 1,
+        'ASPT-122' : 1,
+        'ASPT-136' : 1,
+        'ASPT-145' : 1,
+        'ASPT-153' : 1,
+        'ASPT-154' : 1,
+        'ASPT-161' : 1,
+        'ASPT-178' : 1,
+        'ASPT-185' : 1,
+        'GLUT-14'  : 1,
+        'GLUT-26'  : 0,
+        'GLUT-35'  : 0,
+        'GLUT-67'  : 0,
+        'GLUT-69'  : 0,
+        'GLUT-75'  : 0,
+        'GLUT-82'  : 0,
+        'GLUT-104' : 1,
+        'GLUT-147' : 1,
+        'GLUT-163' : 1,
+        'GLUT-177' : 0,
+        'GLUT-181' : 1,
+        'GLUT-222' : 1,
+        'GLUT-243' : 0,
+        'GLUT-272' : 1,
+        'GLUT-282' : 1
+    }
+
+    nemecz2017 = { # also Hu2018
+        'ASPT-13'  : 1,
+        'ASPT-31'  : 1,
+        'ASPT-32'  : 1,
+        'ASPT-49'  : 1,
+        'ASPT-55'  : 1,
+        'ASPT-86'  : 0,
+        'ASPT-88'  : 0,
+        'ASPT-91'  : 1,
+        'ASPT-97'  : 1,
+        'ASPT-115' : 1,
+        'ASPT-122' : 1,
+        'ASPT-136' : 1,
+        'ASPT-145' : 1,
+        'ASPT-153' : 1,
+        'ASPT-154' : 1,
+        'ASPT-161' : 1,
+        'ASPT-178' : 1,
+        'ASPT-185' : 1,
+        'GLUT-14'  : 1,
+        'GLUT-26'  : 0,
+        'GLUT-35'  : 0,
+        'GLUT-67'  : 1,
+        'GLUT-69'  : 1,
+        'GLUT-75'  : 1,
+        'GLUT-82'  : 1,
+        'GLUT-104' : 1,
+        'GLUT-147' : 1,
+        'GLUT-163' : 1,
+        'GLUT-177' : 1,
+        'GLUT-181' : 1,
+        'GLUT-222' : 0,
+        'GLUT-243' : 0,
+        'GLUT-272' : 1,
+        'GLUT-282' : 1
+    }
+
+    ullman = { # unpublished
+        'ASPT-13'  : 1,
+        'ASPT-31'  : 1,
+        'ASPT-32'  : 1,
+        'ASPT-49'  : 1,
+        'ASPT-55'  : 1,
+        'ASPT-86'  : 1,
+        'ASPT-88'  : 1,
+        'ASPT-91'  : 1,
+        'ASPT-97'  : 1,
+        'ASPT-115' : 1,
+        'ASPT-122' : 1,
+        'ASPT-136' : 1,
+        'ASPT-145' : 1,
+        'ASPT-153' : 1,
+        'ASPT-154' : 1,
+        'ASPT-161' : 1,
+        'ASPT-178' : 1,
+        'ASPT-185' : 1,
+        'GLUT-14'  : 1,
+        'GLUT-26'  : 0,
+        'GLUT-35'  : 0,
+        'GLUT-67'  : 0,
+        'GLUT-69'  : 0,
+        'GLUT-75'  : 0,
+        'GLUT-82'  : 1,
+        'GLUT-104' : 1,
+        'GLUT-147' : 0,
+        'GLUT-163' : 0,
+        'GLUT-177' : 0,
+        'GLUT-181' : 1,
+        'GLUT-222' : 1,
+        'GLUT-243' : 0,
+        'GLUT-272' : 0,
+        'GLUT-282' : 1
+    }
+
+    # DIRECTORY STRUCTURE ######################################################
+    dirname = "lambdaplots" 
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+    else:
+        os.system("rm -f {0}/*.png {0}/*.pdf".format(dirname))
+
+    # GET THE RESIDUE NUMBER, NAME, AND CHAIN OF ALL PROTO RESIDUES ############
+    structure.load(fileName)
+    residues = universe.get('d_residues')
+
+    resnameList = []
+    residList   = []
+    chainList   = []
+    for residue in residues:
+        if residue.d_resname in ["ASPT", "GLUT"]:
+            resnameList.append(residue.d_resname)
+            residList.append(residue.d_resid)
+            chainList.append(residue.d_chain)
+
+    # CREATE LAMBDA PLOT FOR EVERY INDIVIDUAL PROTONATABLE RESIDUE #############
+
+    # Loop through all the lambdas:
+    for idx in range(1, len(resnameList) + 1):
+
+        plt.figure(figsize=(8, 6))
+
+        # Update user
+        print("plotting {}/{}".format(idx, len(resnameList)), end='\r')
+        
+        # Load columns from .dat files
+        t = loadCol("lambda_{0}.dat".format(idx), 1)
+        x = loadCol("lambda_{0}.dat".format(idx), 2)
+        
+        # If we had a crash, then the last line may not have been completely written.
+        if len(t) > len(x):
+            t.pop()
+        elif len(t) < len(x):
+            x.pop()
+
+        plt.plot(t, x, linewidth=0.5)
+
+        # Title
+        plt.title("{0}-{1} in chain {2} in {3}.pdb\npH={4}, nstlambda={5}, deprotonation={6:.2f}".format(
+            resnameList[idx - 1],
+            residList[idx - 1],
+            chainList[idx - 1],
+            pdbName,
+            pH,
+            nstOut,
+            titrate("lambda_{}.dat".format(idx))
+        ))
+
+        # Axes and stuff
+        plt.ylim(-0.1, 1.1)
+        plt.xlabel("Time (ps)")
+        plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 3))
+        plt.ylabel(r"$\lambda$-coordinate")
+        plt.grid()
+
+        # Save.
+        fileName = "{}/{}_{}-{:03d}".format(dirname, chainList[idx-1], resnameList[idx-1], residList[idx-1])
+        # plt.savefig("{}.pdf".format(fileName)); os.system("pdfcrop {0}.pdf {0}.pdf >> /dev/null 2>&1".format(fileName))
+        plt.savefig("{}.png".format(fileName))
+
+        # clf = clear the entire current figure. close = closes a window.
+        plt.clf(); plt.close()
+
+    # CREATE HISTOGRAM PLOTS FOR COMBINED PROTO STATE OF ALL FIVE CHAINS #######
+    number_of_chains   = len(set(chainList))
+    residues_per_chain = int(len(resnameList) / number_of_chains)
+    
+    for ii in range(1, residues_per_chain + 1):
+        data = []        
+        for jj in range(0, number_of_chains):
+            print(ii + residues_per_chain * jj, end=' ')
+            # data += (loadCol('lambda_{}.dat'.format(ii + residues_per_chain * jj), 2, 49713, 124320))
+            data += (loadCol('lambda_{}.dat'.format(ii + residues_per_chain * jj), 2))
+        print()
+
+        # PLOTTING STUFF #######################################################
+
+        plt.figure(figsize=(8, 6))
+        plt.hist(data, density=True, bins=200)
+        
+        # Title
+        plt.title("{0}-{1} (all chains) in {2}.pdb\npH={3}, nstlambda={4}, deprotonation={5:.2f}".format(
+            resnameList[ii-1],
+            residList[ii-1],
+            pdbName,
+            pH,
+            nstOut,
+            titrate("lambda_{}.dat".format(ii))
+            # expVals40["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]
+            ))
+
+        # Axes and stuff
+        plt.axis([-0.1, 1.1, -0.1, 12])
+        plt.xlabel(r"$\lambda$-coordinate")
+        plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 3))
+        plt.grid()
+
+        # Add green vertical line indicating experimental value
+        plt.vlines(x=biophys["{0}-{1}".format(resnameList[ii-1], residList[ii-1])], ymin=0, ymax=12, color='r', linewidth=4.0, label="biophysics.se/Prevost2012 = {}".format(biophys["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]))
+        plt.vlines(x=nury2010["{0}-{1}".format(resnameList[ii-1], residList[ii-1])], ymin=0, ymax=10, color='g', linewidth=4.0, label="Nury2010/Cheng2010/Calimet2013 = {}".format(nury2010["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]))
+        plt.vlines(x=fritsch2011["{0}-{1}".format(resnameList[ii-1], residList[ii-1])], ymin=0, ymax=8, color='b', linewidth=4.0, label="Fritsch2011 = {}".format(fritsch2011["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]))
+        plt.vlines(x=lev2017["{0}-{1}".format(resnameList[ii-1], residList[ii-1])], ymin=0, ymax=6, color='c', linewidth=4.0, label="Lev2017 = {}".format(lev2017["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]))
+        plt.vlines(x=nemecz2017["{0}-{1}".format(resnameList[ii-1], residList[ii-1])], ymin=0, ymax=4, color = 'm', linewidth=4.0, label="Nemecz2017/Hu2018 = {}".format(nemecz2017["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]))
+        plt.vlines(x=ullman["{0}-{1}".format(resnameList[ii-1], residList[ii-1])], ymin=0, ymax=2, color='y', linewidth=4.0, label="Ullman (unpublished) = {}".format(ullman["{0}-{1}".format(resnameList[ii-1], residList[ii-1])]))
+
+        plt.legend()
+        # Save and clear
+        fileName = "{}/hist_{}-{:03d}".format(dirname, resnameList[ii-1], residList[ii-1])
+        # plt.savefig("{}.pdf".format(fileName)); os.system("pdfcrop {0}.pdf {0}.pdf >> /dev/null 2>&1".format(fileName))
+        plt.savefig('{}.png'.format(fileName))
+        plt.clf(); plt.close()
+
+################################################################################
+
+# compareLambdaFiles(['lambda_1.dat', 'lambda_2.dat', 'lambda_3.dat', 'lambda_7.dat'])
+# compareLambdaFiles(['lambda_4.dat', 'lambda_5.dat', 'lambda_6.dat', 'lambda_7.dat'])
+# compareLambdaFiles(['lambda_1.dat', 'lambda_2.dat', 'lambda_3.dat', 'lambda_4.dat', 'lambda_5.dat', 'lambda_6.dat', 'lambda_7.dat'])
+# compareLambdaFiles(['lambda_1.dat', 'lambda_2.dat', 'lambda_3.dat', 'lambda_216.dat'])
