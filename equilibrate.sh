6d0d26bdf9da5a3accfd51476dbb2316abae714e
Anton Jansen <antonjansen96@gmail.com>
2021-10-15 12:23:44 +0200

    EM now also works with cpHMD

--- equilibrate.sh
+++ equilibrate.sh
@@ -3,8 +3,8 @@
 # Gromacs version to use:
 source /usr/local/gromacs_constantph/bin/GMXRC
 
-gmx grompp -f EM.mdp -c phneutral.pdb -p topol.top -o EM.tpr -maxwarn 1
-gmx mdrun -v -deffnm EM -c EM.pdb
+gmx grompp -f EM.mdp -c phneutral.pdb -p topol.top -n index.ndx -o EM.tpr -maxwarn 2
+gmx mdrun -deffnm EM -c EM.pdb
 
 gmx grompp -f NVT.mdp -c EM.pdb -p topol.top -n index.ndx -o NVT.tpr -r EM.pdb -maxwarn 1
 gmx mdrun -v -deffnm NVT -c NVT.pdb -notunepme
--- prep.sh
+++ prep.sh
@@ -2,16 +2,9 @@
 
 echo 1 | phbuilder gentopol -f proteins/1cvo.pdb
 
-# echo phbuilder : adding BOX...
 gmx editconf -f phprocessed.pdb -o box.pdb -bt cubic -d 1.5
 
-# echo phbuilder : adding Solvent...
 gmx solvate -cp box.pdb -p topol.top -o solvated.pdb
 
-# echo phbuilder : adding Ions...
-touch IONS.mdp
-gmx grompp -f IONS.mdp -c solvated.pdb -p topol.top -o IONS.tpr
-echo SOL | gmx genion -s IONS.tpr -o ions.pdb -p topol.top -neutral
-
-phbuilder addbuffers -f ions.pdb -o buffers.pdb -nbufs 10
-phbuilder genparams -f buffers.pdb -ph 4.0
+phbuilder neutralize -f solvated.pdb
+phbuilder genparams -f phneutral.pdb -ph 4.0
