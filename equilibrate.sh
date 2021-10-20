4065cf574537e532d19c89b2101bf271b5293dc4
Anton Jansen <antonjansen96@gmail.com>
2021-10-20 18:34:36 +0200

    use updated guess for buffers

--- equilibrate.sh
+++ equilibrate.sh
@@ -4,7 +4,7 @@
 source /usr/local/gromacs_constantph/bin/GMXRC
 
 gmx grompp -f EM.mdp -c phneutral.pdb -p topol.top -n index.ndx -o EM.tpr -maxwarn 2
-gmx mdrun -deffnm EM -c EM.pdb
+gmx mdrun -v -deffnm EM -c EM.pdb
 
 gmx grompp -f NVT.mdp -c EM.pdb -p topol.top -n index.ndx -o NVT.tpr -r EM.pdb -maxwarn 1
 gmx mdrun -v -deffnm NVT -c NVT.pdb -notunepme
