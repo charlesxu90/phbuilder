b6b57f9962a05e47431d015fc108e87ebba010df
Anton Jansen <antonjansen96@gmail.com>
2022-08-22 18:42:37 +0200

    updated script

--- misc/GLIC/doPanels.py
+++ misc/GLIC/doPanels.py
@@ -33,14 +33,16 @@ def loadxvg(fname, col=[0, 1], dt=1, b=0):
             data[idx].append(float(listLine[col[idx]]))
     return data
 
-# Handle user input.
 def inputOptionHandler(message, options):
-    '''Handles user input'''
+    """
+    Function for handling user input.
+    message: the string you would like to prompt the user.
+    options: a list of strings containing the options.
+    """
 
     valids = []
     msgstring = "{}:".format(message)
 
-    # Loop through the options list and create string for dislay
     for idx in range(0, len(options)):
         msgstring += "\n{}. {}".format(idx, options[idx])
         valids.append(str(idx))
@@ -88,6 +90,8 @@ def getLambdaFileIndices(structure, residue):
 
 ################################################################################
 
+# MDANALYSIS DOES NOT KNOW WHAT THE BACKBONE OF ASPT GLUT HSPT ARE!!!!!
+
 for simulation in ['4HFI_4']:
 # for sim in ['4HFI_4', '4HFI_7', '6ZGD_4', '6ZGD_7']:
 
@@ -97,64 +101,88 @@ for simulation in ['4HFI_4']:
         chain = ['A', 'B', 'C', 'D', 'E']
         array = getLambdaFileIndices('{}/01/CA.pdb'.format(simulation), residue)
 
+        store = []
         for idx in range(0, len(array)):
-            data = loadxvg('{}/01/cphmd-coord-{}.xvg'.format(simulation, array[idx]), dt=1000, b=0)
+            data = loadxvg('{}/01/cphmd-coord-{}.xvg'.format(simulation, array[idx]), dt=5000, b=0)
             t    = [val / 1000.0 for val in data[0]] # ps -> ns
             x    = [1.0 - val for val in data[1]] # deprotonation -> protonation
+            store.append(x)
 
             plt.plot(t, x, linewidth=0.5, label=chain[idx])
 
+        # PLOT MEAN PROTONATION
+        average = [0] * len(store[0])
+        for idx in range(0, len(store[0])):
+            average[idx] = store[0] + store[1] + store[2] + store[3] + store[4]
+        plt.plot(t, x, linewidth=1.5, label='mean', color='b', linestyle=':')
+
         plt.ylabel('Protonation')
         plt.xlabel('Time (ns)')
+        plt.axis([0, 1000, -0.1, 1.1])
+        plt.title('{} protonation {}'.format(simulation, residue))
         plt.legend()
         plt.tight_layout()
-        plt.savefig('panels/{}_{}.png'.format(simulation, residue))
+        plt.savefig('panels/proto_{}_{}.png'.format(simulation, residue))
         plt.clf(); plt.close()
 
     # GET THE RMSD OF SPECIFIED SECTION(S) VERSUS TIME #########################
 
-    if inputOptionHandler('Run rms analysis for {} again?'.format(simulation), ['no', 'yes']):
+    path1 = '{}/01/CA.pdb'.format(simulation)
+    path2 = '{}/01/MD_whole.xtc'.format(simulation)
+    u = MDAnalysis.Universe(path1, path2)
 
-        u = MDAnalysis.Universe('{}/01/CA.pdb'.format(simulation), '{}/01/MD.xtc'.format(simulation))
-
-        R = MDAnalysis.analysis.rms.RMSD(u, select='(segid A B C D E) and backbone and resid 32-35')
-        R.run(step=100) # speed things up a bit
-
+    # INDIVIDUAL CHAINS
+    chain = ['A', 'B', 'C', 'D', 'E']
+    for idx in range(0, len(chain)):
+        R = MDAnalysis.analysis.rms.RMSD(u, select='segid {} and resid 32-35'.format(chain[idx]))
+        R.run()
         t  = [val / 1000.0 for val in R.rmsd.T[1]]
         x1 = R.rmsd.T[2]
+        plt.plot(t, x1, label=chain[idx])
 
-        plt.plot(t, x1)
-        plt.xlabel("time (ns)")
-        plt.ylabel(r"RMSD ($\AA$)")
-        plt.legend()
-        plt.tight_layout()
-        plt.savefig('panels/{}_{}-{}.png'.format(simulation, 32, 35))
-        plt.clf(); plt.close()
-
-    # MAKE THE MINDIST PLOT FOR SPECIFIED RESIDUE COMBINATION ##################
-    
-    u  = MDAnalysis.Universe('{}/01/CA.pdb'.format(simulation), '{}/01/MD.xtc'.format(simulation))
-
-    selA = u.select_atoms('segid A and resid 243')
-    selB = u.select_atoms('segid A and resid 248')
-
-    t = []
-    d = []
-    for ts in u.trajectory[::100]:
-        com1 = selA.center_of_mass()
-        com2 = selB.center_of_mass()
-        
-        t.append(u.trajectory.time)
-        d.append(np.linalg.norm(com1 - com2))
-
-    t = [val / 1000.0 for val in t] # ps -> ns
-
-    plt.plot(t, d)
-    # plt.plot(t, d, legend='243-248 chain A')
+    # ALL CHAINS
+    R = MDAnalysis.analysis.rms.RMSD(u, select='(segid A B C D E) and resid 32-35')
+    R.run()
+    t  = [val / 1000.0 for val in R.rmsd.T[1]]
+    x1 = R.rmsd.T[2]
+    plt.plot(t, x1, label='all')
 
     plt.xlabel("time (ns)")
-    plt.ylabel(r"Minimum distance ($\AA$)")
+    plt.ylabel(r"RMSD ($\AA$)")
+    plt.xlim(0, 1000)
+    plt.title('{} RMSD segment {}-{}'.format(simulation, 32, 35))
     plt.legend()
     plt.tight_layout()
-    plt.savefig('panels/{}_{}-{}_dist.png'.format(simulation, 243, 248))
+    plt.savefig('panels/rmsd_{}_{}-{}.png'.format(simulation, 32, 35))
+    plt.clf(); plt.close()
+
+    # MAKE THE MINDIST PLOT FOR SPECIFIED RESIDUE COMBINATION ##################
+
+    u  = MDAnalysis.Universe('{}/01/CA.pdb'.format(simulation), '{}/01/MD.xtc'.format(simulation))
+
+    chain = ['A', 'B', 'C', 'D', 'E']
+    for idx in range(0, len(chain)):
+
+        selA = u.select_atoms('segid {} and resid 243'.format(chain[idx]))
+        selB = u.select_atoms('segid {} and resid 248'.format(chain[idx]))
+
+        t = []; d = []
+        for ts in u.trajectory[::100]:
+            com1 = selA.center_of_mass()
+            com2 = selB.center_of_mass()
+
+            t.append(u.trajectory.time)
+            d.append(np.linalg.norm(com1 - com2))
+
+        t = [val / 1000.0 for val in t] # ps -> ns
+
+        plt.plot(t, d, label='243-248 chain {}'.format(chain[idx]))
+
+    plt.xlabel("time (ns)")
+    plt.xlim(0, 1000)
+    plt.ylabel(r"Minimum distance ($\AA$)")
+    plt.title('{} minidst 243-248'.format(simulation))
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('panels/mindst_{}_{}-{}_dist.png'.format(simulation, 243, 248))
     plt.clf(); plt.close()
