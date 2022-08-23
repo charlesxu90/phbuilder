8faa27a5ded9fd70250a248d95924e23f77606c7
Anton Jansen <antonjansen96@gmail.com>
2022-08-23 14:28:50 +0200

    change to functions

--- misc/GLIC/doPanels.py
+++ misc/GLIC/doPanels.py
@@ -2,9 +2,10 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 import MDAnalysis
 import MDAnalysis.analysis.rms
+
+# MDANALYSIS DOES NOT KNOW WHAT THE BACKBONE OF ASPT GLUT HSPT ARE!!!!!
 
 def loadxvg(fname, col=[0, 1], dt=1, b=0):
     """
@@ -88,60 +89,64 @@ def getLambdaFileIndices(structure, residue):
         elif array[idx][0] == 'H':
             count += 3
 
-################################################################################
+def chargePlot(sim, rep, res):
+    """
+    Make the charge plot in time for residue. Does not work for histidines.
+    sim: the simulation, e.g. 4HFI_4.
+    rep: the replica, e.g. 1.
+    res: the residue, e.g. E35.
+    """
 
-# MDANALYSIS DOES NOT KNOW WHAT THE BACKBONE OF ASPT GLUT HSPT ARE!!!!!
+    chain = ['A', 'B', 'C', 'D', 'E']
+    array = getLambdaFileIndices('{}/{:02d}/CA.pdb'.format(sim, rep), res)
 
-for simulation in ['4HFI_4']:
-# for sim in ['4HFI_4', '4HFI_7', '6ZGD_4', '6ZGD_7']:
+    store = []
+    for idx in range(0, len(array)):
+        data = loadxvg('{}/{:02d}/cphmd-coord-{}.xvg'.format(sim, rep, array[idx]), dt=5000, b=0)
+        t    = [val / 1000.0 for val in data[0]] # ps -> ns
+        x    = [1.0 - val for val in data[1]] # deprotonation -> protonation
+        store.append(x)
 
-    # GET THE CHARGES ON SPECIFIED RESIDUE(S) AND CHAIN(S) VERSUS TIME #########
+        plt.plot(t, x, linewidth=0.5, label=chain[idx])
 
-    for residue in ['D32', 'E35']:
-        chain = ['A', 'B', 'C', 'D', 'E']
-        array = getLambdaFileIndices('{}/01/CA.pdb'.format(simulation), residue)
+    # PLOT MEAN PROTONATION
+    average = [0] * len(store[0])
+    for idx in range(0, len(store[0])):
+        average[idx] = store[0] + store[1] + store[2] + store[3] + store[4]
+    plt.plot(t, x, linewidth=1.5, label='mean', color='b', linestyle=':')
 
-        store = []
-        for idx in range(0, len(array)):
-            data = loadxvg('{}/01/cphmd-coord-{}.xvg'.format(simulation, array[idx]), dt=5000, b=0)
-            t    = [val / 1000.0 for val in data[0]] # ps -> ns
-            x    = [1.0 - val for val in data[1]] # deprotonation -> protonation
-            store.append(x)
+    plt.ylabel('Protonation')
+    plt.xlabel('Time (ns)')
+    plt.axis([0, 1000, -0.1, 1.1])
+    plt.title('{} protonation {}'.format(sim, res))
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('panels/proto_{}_{}.png'.format(sim, res))
+    plt.clf(); plt.close()
 
-            plt.plot(t, x, linewidth=0.5, label=chain[idx])
+def RMSDPlot(sim, rep, sel):
+    """
+    Creates RMSD plots for a selection of residues. Load MD_whole.xtc.
+    sim: the simulation, e.g. 4HFI_4.
+    rep: the replica, e.g. 1.
+    sel: selection, e.g. '32-35'
+    """
 
-        # PLOT MEAN PROTONATION
-        average = [0] * len(store[0])
-        for idx in range(0, len(store[0])):
-            average[idx] = store[0] + store[1] + store[2] + store[3] + store[4]
-        plt.plot(t, x, linewidth=1.5, label='mean', color='b', linestyle=':')
-
-        plt.ylabel('Protonation')
-        plt.xlabel('Time (ns)')
-        plt.axis([0, 1000, -0.1, 1.1])
-        plt.title('{} protonation {}'.format(simulation, residue))
-        plt.legend()
-        plt.tight_layout()
-        plt.savefig('panels/proto_{}_{}.png'.format(simulation, residue))
-        plt.clf(); plt.close()
-
-    # GET THE RMSD OF SPECIFIED SECTION(S) VERSUS TIME #########################
-
-    path1 = '{}/01/CA.pdb'.format(simulation)
-    path2 = '{}/01/MD_whole.xtc'.format(simulation)
+    path1 = '{}/{:02d}/CA.pdb'.format(sim, rep)
+    path2 = '{}/{:02d}/MD_whole.xtc'.format(sim, rep)
     u = MDAnalysis.Universe(path1, path2)
 
     # INDIVIDUAL CHAINS
     chain = ['A', 'B', 'C', 'D', 'E']
     for idx in range(0, len(chain)):
-        R = MDAnalysis.analysis.rms.RMSD(u, select='segid {} and resid 32-35'.format(chain[idx]))
+        R = MDAnalysis.analysis.rms.RMSD(u, select='segid {} and resid {}'.format(chain[idx], sel))
         R.run()
         t  = [val / 1000.0 for val in R.rmsd.T[1]]
         x1 = R.rmsd.T[2]
         plt.plot(t, x1, label=chain[idx])
 
     # ALL CHAINS
-    R = MDAnalysis.analysis.rms.RMSD(u, select='(segid A B C D E) and resid 32-35')
+    R = MDAnalysis.analysis.rms.RMSD(u, select='(segid A B C D E) and resid {}'.format(sel))
     R.run()
     t  = [val / 1000.0 for val in R.rmsd.T[1]]
     x1 = R.rmsd.T[2]
@@ -150,21 +155,30 @@ for simulation in ['4HFI_4']:
     plt.xlabel("time (ns)")
     plt.ylabel(r"RMSD ($\AA$)")
     plt.xlim(0, 1000)
-    plt.title('{} RMSD segment {}-{}'.format(simulation, 32, 35))
+    plt.title('{} RMSD segment {}'.format(sim, sel))
     plt.legend()
     plt.tight_layout()
-    plt.savefig('panels/rmsd_{}_{}-{}.png'.format(simulation, 32, 35))
+    plt.savefig('panels/rmsd_{}_{}.png'.format(sim, sel))
     plt.clf(); plt.close()
 
-    # MAKE THE MINDIST PLOT FOR SPECIFIED RESIDUE COMBINATION ##################
+def mindistPlot(sim, rep, sel1, sel2, name):
+    """
+    Creates mindists plots between sel1 and sel2.
+    sim: the simulation, e.g. 4HFI_4.
+    rep: the replica, e.g. 1.
+    sel1: selection1, e.g. 'resid 243'
+    sel2: selection2, e.g. 'resid 248'
+    """
 
-    u  = MDAnalysis.Universe('{}/01/CA.pdb'.format(simulation), '{}/01/MD.xtc'.format(simulation))
+    path1 = '{}/{:02d}/CA.pdb'.format(sim, rep)
+    path2 = '{}/{:02d}/MD.xtc'.format(sim, rep)
+    u  = MDAnalysis.Universe(path1, path2)
 
     chain = ['A', 'B', 'C', 'D', 'E']
     for idx in range(0, len(chain)):
 
-        selA = u.select_atoms('segid {} and resid 243'.format(chain[idx]))
-        selB = u.select_atoms('segid {} and resid 248'.format(chain[idx]))
+        selA = u.select_atoms('segid {} and {}'.format(chain[idx], sel1))
+        selB = u.select_atoms('segid {} and {}'.format(chain[idx], sel2))
 
         t = []; d = []
         for ts in u.trajectory[::100]:
@@ -176,13 +190,18 @@ for simulation in ['4HFI_4']:
 
         t = [val / 1000.0 for val in t] # ps -> ns
 
-        plt.plot(t, d, label='243-248 chain {}'.format(chain[idx]))
+        plt.plot(t, d, label='chain {}'.format(chain[idx]))
 
     plt.xlabel("time (ns)")
     plt.xlim(0, 1000)
     plt.ylabel(r"Minimum distance ($\AA$)")
-    plt.title('{} minidst 243-248'.format(simulation))
+    plt.title('{} minidst {}'.format(sim, name))
     plt.legend()
     plt.tight_layout()
-    plt.savefig('panels/mindst_{}_{}-{}_dist.png'.format(simulation, 243, 248))
+    plt.savefig('panels/mindst_{}_{}.png'.format(sim, name))
     plt.clf(); plt.close()
+
+chargePlot('4HFI_4', 1, 'D32')
+chargePlot('4HFI_4', 1, 'E35')
+RMSDPlot('4HFI_4', 1, '32-35')
+mindistPlot('4HFI_4', 1, 'resid 243', 'resid 248', 'E243-K248')
