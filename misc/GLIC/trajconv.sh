b2dc8ad34509a1d0d10f261b13394e41117cb88a
Anton Jansen <antonjansen96@gmail.com>
2022-08-24 11:25:58 +0200

    bug fix: this is correct way to convert the traj

--- misc/GLIC/trajconv.sh
+++ misc/GLIC/trajconv.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
+source /usr/local/gromacs_constantph/bin/GMXRC
+
 for sim in 4HFI_4 4HFI_7 6ZGD_4 6ZGD_7
 do
     cd $sim
     for rep in 01 02 03 04
     do
         cd $rep
-        echo 0 | gmx trjconv -f MD.xtc -o MD_whole.xtc -s MD.tpr -e 1000000 -dt 1000 -pbc whole
+        
+        echo 0   | gmx trjconv -s MD.tpr -n index.ndx -f MD.xtc -o A.xtc -pbc nojump -dt 1000 -e 1000000
+        echo 1 0 | gmx trjconv -s MD.tpr -n index.ndx -f A.xtc -o B.xtc -center
+        echo 0   | gmx trjconv -s MD.tpr -n index.ndx -f B.xtc -o MD_conv.xtc -pbc mol
+        rm A.xtc B.xtc
+
         rm -f \#*\#
         cd ..
     done
