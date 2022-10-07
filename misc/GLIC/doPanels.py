8ebc0fcbaa449afacca854d8eba966436272100e
Anton Jansen <antonjansen96@gmail.com>
2022-10-07 17:58:17 +0200

    mindistPlot now part of class

--- misc/GLIC/doPanels.py
+++ misc/GLIC/doPanels.py
@@ -159,54 +159,6 @@ def RMSDPlot(sim, rep, sel):
     plt.savefig('panels/rmsd_{}_{}.png'.format(sim, sel))
     plt.clf(); plt.close()
 
-def mindistPlot(sim, rep, resid1, resid2, chain1, chain2, name):
-    """
-    Creates mindists plots between sel1 and sel2. Uses MD_conv.xtc.
-    sim: the simulation, e.g. '4HFI_4'
-    rep: the replica, e.g. 1
-    resid1: residue 1, e.g. 35
-    resid1: residue 2, e.g. 158
-    chain1: chains to match e.g. ['A', 'B', 'C', 'D', 'E']
-    chain2: chains to match e.g. ['E', 'A', 'B', 'C', 'D']
-    name: file/title name, e.g. 'E35-T158'
-    """
-
-    if len(chain1) != len(chain2):
-        raise Exception('chain1 and chain2 must be same length')
-
-    os.chdir('{}/{:02d}'.format(sim, rep))
-
-    stdin = ['q']
-    for chain in ['A', 'B', 'C', 'D', 'E'][::-1]:
-        stdin.insert(0, 'r {} & chain {}'.format(resid1, chain))
-        stdin.insert(0, 'r {} & chain {}'.format(resid2, chain))
-
-    gromacs('make_ndx -f CA.pdb -o mindist.ndx', stdin=stdin)
-
-    for idx in range(0, len(chain1)):
-        sel1 = 'r_{}_&_ch{}'.format(resid1, chain1[idx])
-        sel2 = 'r_{}_&_ch{}'.format(resid2, chain2[idx])
-
-        gromacs('mindist -s MD.tpr -f MD_conv.xtc -n mindist.ndx -dt 10', stdin=[sel1, sel2])
-
-        data = loadxvg('mindist.xvg')
-        t = [val / 1000.0 for val in data[0]]
-        x = data[1]
-        plt.plot(t, x, linewidth=0.5, label='{}-{}'.format(chain1[idx], chain2[idx]))
-
-    os.system('rm -f mindist.ndx \\#*\\#')
-    os.chdir('../..')
-
-    plt.xlabel("time (ns)")
-    plt.xlim(0, 1000)
-    plt.ylim(0, 1)
-    plt.ylabel("Minimum distance (nm)")
-    plt.title('{} mindist {}'.format(sim, name))
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig('panels/mindst_{}_{}.png'.format(sim, name))
-    plt.clf(); plt.close()
-
 def mindistIonsPlot(sim, rep, resid, ion, name):
     """
     Creates mindists plots between sel1 and sel2. Uses MD_conv.xtc.
@@ -255,7 +207,7 @@ class PanelBuilder:
     """
     Combines multiple functions and tries to create the entire panel at once.
     target: <int> the target resid e.g. 35
-    resids: <list> the contacts e.g. [158, 'NA']
+    resids: <list> the contacts you'd like to check, e.g. [158c, 'NA']
     rmsd: <string> optional, e.g. 'resid 15 to 22'
     test: <bool> is this a test run? (faster)
     """
@@ -278,13 +230,24 @@ class PanelBuilder:
 
                 # CREATE THE CHARGE PLOTS
                 if notExists("proto_{}_{}.png".format(sim, self.target)):
-                    self.chargePlot(sim, rep, self.target)
+                    self.chargePlot(sim, rep)
+                
+                # CREATE THE MINIMUM DISTANCE PLOTS
+                for resid in self.resids:
+                    if notExists('mindist_{}_{}-{}.png'.format(sim, self.target, resid)):
+                        self.mindistPlot(sim, rep, resid)
 
+                # CREATE THE RMSD PLOTS
+                if self.rmsd != '':
+                    if notExists('rmsd_{}_{}.png'.format(sim, self.target)):
+                        self.RMSDPlot(sim, rep, sel=self.rmsd)
+
+        # STUFF RELATED TO THE PANELS
         self.rowCount = 0
         self.createPanelRows()
         self.createPanelColumns()
 
-    def chargePlot(self, sim, rep, resid):
+    def chargePlot(self, sim, rep):
         """
         Make the charge plot in time for residue. Does not work for histidines.
         sim: the simulation, e.g. '4HFI_4'
@@ -294,7 +257,7 @@ class PanelBuilder:
         print('Creating charge plot')
 
         chain = ['A', 'B', 'C', 'D', 'E']
-        array = getLambdaFileIndices('{}/{:02d}/CA.pdb'.format(sim, rep), resid)
+        array = getLambdaFileIndices('{}/{:02d}/CA.pdb'.format(sim, rep), self.target)
 
         store = []
         for idx in range(0, len(array)):
@@ -314,11 +277,80 @@ class PanelBuilder:
         plt.ylabel('Protonation')
         plt.xlabel('Time (ns)')
         plt.axis([0, 1000, -0.1, 1.1])
-        plt.title('{} protonation {}'.format(sim, resid))
+        plt.title('{} protonation {}'.format(sim, self.target))
         plt.legend()
         plt.tight_layout()
-        plt.savefig('panels/proto_{}_{}.png'.format(sim, resid))
+        plt.savefig('panels/proto_{}_{}.png'.format(sim, self.target))
         plt.clf(); plt.close()
+
+    def mindistPlot(self, sim, rep, resid):
+        """
+        Currently a wrapper for GROMACS mindist. Makes the minimum distance plots in time.
+        sim: the simulation, e.g. '4HFI_4'
+        rep: the replica, e.g. 1
+        resid: the residue it makes contacts with, e.g. 158c
+        """
+        print("Creating mindist plot")
+
+        # Go to the simulation directory
+        os.chdir('{}/{:02d}'.format(sim, rep))
+
+        # Process the principal vs complementary identifier.
+        # Note: 158c gives the correct distances using these lists, and 158 is 
+        # in fact complementary to 35 so these chain2 orders are correct.
+
+        chain1 = ['A','B','C','D','E']
+        if resid[-1] == 'c':
+            chain2 = ['E','A','B','C','D']
+            xyz = int(resid[:-1])
+        elif resid[-1] == 'p':
+            chain2 = ['B','C','D','E','A']
+            xyz = int(resid[:-1])
+        else:
+            chain2 = ['A','B','C','D','E']
+            xyz = int(resid)
+
+        # Create the index file required for the analysis
+        stdin = ['q']
+        for chain in ['A', 'B', 'C', 'D', 'E'][::-1]:
+            stdin.insert(0, 'r {} & chain {}'.format(self.target, chain))
+            stdin.insert(0, 'r {} & chain {}'.format(xyz, chain))
+
+        gromacs('make_ndx -f CA.pdb -o mindist.ndx', stdin=stdin)
+
+        # Call GROMACS mindist using the mindist.ndx we just created:
+        for idx in range(0, len(chain1)):
+            sel1 = 'r_{}_&_ch{}'.format(self.target, chain1[idx])
+            sel2 = 'r_{}_&_ch{}'.format(xyz, chain2[idx])
+
+            print(self.test)
+
+            if self.test:
+                # Speed things up significantly if this is just a test run.
+                gromacs('mindist -s MD.tpr -f MD_conv.xtc -n mindist.ndx -dt 10000', stdin=[sel1, sel2])
+            else:
+                gromacs('mindist -s MD.tpr -f MD_conv.xtc -n mindist.ndx -dt 10', stdin=[sel1, sel2])
+
+            data = loadxvg('mindist.xvg')
+            t = [val / 1000.0 for val in data[0]]
+            x = data[1]
+            plt.plot(t, x, linewidth=0.5, label='{}-{}'.format(chain1[idx], chain2[idx]))
+
+        os.system('rm -f mindist.ndx \\#*\\#')
+        os.chdir('../..')
+
+        plt.xlabel("time (ns)")
+        plt.xlim(0, 1000)
+        plt.ylim(0, 1)
+        plt.ylabel("Minimum distance (nm)")
+        plt.title('{} mindist {}-{}'.format(sim, self.target, resid))
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig('panels/mindist_{}_{}-{}.png'.format(sim, self.target, resid))
+        plt.clf(); plt.close()
+
+    def RMSDPlot(self, sel):
+        pass
 
     def createPanelRows(self):
         """Creates the (temporary) panel rows."""
