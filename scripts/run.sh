2a9da16f90168bf63c3394d2184a348c1f5fd51a
Anton Jansen <antonjansen96@gmail.com>
2023-07-27 14:56:36 +0200

    remove scripts that don't belong here

--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Gromacs version to use:
-source /usr/local/gromacs_constantph/bin/GMXRC
-
-gmx grompp -f MD.mdp -c NPT.gro -p topol.top -n index.ndx -o MD.tpr -maxwarn 1
-gmx mdrun -v -deffnm MD -c MD.pdb -x MD.xtc 
